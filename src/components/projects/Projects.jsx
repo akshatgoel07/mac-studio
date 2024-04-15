f95afbab8d46b09894612f9ce686de4a914b028a
akshato7 <akshathg7@gmail.com>
2024-04-15 22:29:53 +0530

    updated images

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Projects.css";
-import project1Image from '../../assets/buzz.png'
-import projectImage2 from '../../assets/django.png'
-import projectImage3 from '../../assets/phd.png'
-import projectImage4 from '../../assets/codeops.png'
+import projectImage4 from '../../assets/fiverr.jpg'
+import projectImage2 from '../../assets/fiverr-1.jpg'
+import projectImage3 from '../../assets/fiverr-2.jpg'
+import project1Image from '../../assets/fiverr-3.jpg'
 
 const Projects = () => {
     const projectImages = [
-        { src: projectImage4, caption: "CodeOps: Complier Designer" },
-        { src: projectImage2, caption: "Django Automation Software" },
-        { src: projectImage3, caption: "Univeristy PHD Program WebApp" },
-        { src: project1Image, caption: "Buzz" }
+        { src: projectImage4, caption: "We craft the perfect plan to bring your vision to life." },
+        { src: projectImage2, caption: "Building your solution with innovation and precision." },
+        { src: projectImage3, caption: " Seamlessly releasing your creation to the world." },
+        { src: project1Image, caption: "Keeping your project running smoothly and adapting to change." }
     ];
 
     return (
